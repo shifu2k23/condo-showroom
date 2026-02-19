55c3dab004ecde47c6126244f68a7235a0c4f589
Bien <benregidor@gmail.com>
2026-02-19 20:17:46 +0800

    fix: harden mysql ssl ca config and sanitize env example

--- config/database.php
+++ config/database.php
@@ -57,9 +57,9 @@ return [
             'prefix_indexes' => true,
             //'strict' => true,
             //'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? [
-                PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt',
-            ] : [],
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         'mariadb' => [
